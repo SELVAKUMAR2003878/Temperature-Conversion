6907a6fd4c6315bb0f30ea7e9a30061960d5dd55
SELVAKUMAR2003878 <selvakumarnader023@gmail.com>
2024-09-24 22:34:29 +0530

    First Commit
    First Date: 24-09-2024

---
+++ index.js
@@ -0,0 +1,25 @@
+var inputvalue = document.getElementById("input");
+
+var first = document.getElementById("first");
+var second = document.getElementById("second");
+var result = document.getElementById("result");
+
+document.getElementById("btn").onclick = function() {
+
+  if(inputvalue.value == 0) {
+    window.alert("you are not enter in any value");
+  }
+
+  else if(first.checked){
+    temp = (9 * inputvalue.value + 160)/5;
+    result.textContent = temp + " F";
+  }
+  else if(second.checked) {
+    temp = Math.ceil((5 * (inputvalue.value - 32))/9);
+    result.textContent = temp + " C";
+  }
+  else{
+    result.textContent = "You are not select in any type";
+  }
+  
+}
